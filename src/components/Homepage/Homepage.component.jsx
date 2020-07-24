e8ca65553b8c9ef92d706bc67e62a11a30a65a84
subham saurabh <saurabhsubham113@gmail.com>
2020-07-24 23:34:13 +0530

    menu item transition

---
+++ src/components/Homepage/Homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Directory from '../Directory/Directory.component'
+import './Homepage.style.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
+
+export default HomePage;
